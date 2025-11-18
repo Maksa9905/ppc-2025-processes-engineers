297d02b84a1ab3431aaaf4316eccb292c6fdccd8
hakolr <m.gayvoronskiy@flang.io>
2025-11-19 02:20:46 +0300

    fix: fixed tests name

--- tasks/gaivoronskiy_m_average_vector_sum/tests/functional/main.cpp
+++ tasks/gaivoronskiy_m_average_vector_sum/tests/functional/main.cpp
@@ -20,7 +20,7 @@
 
 namespace gaivoronskiy_m_average_vector_sum {
 
-class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class GaivoronkiyMRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
     return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
@@ -65,7 +65,7 @@ class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType
 
 namespace {
 
-TEST_P(NesterovARunFuncTestsProcesses, MatmulFromPic) {
+TEST_P(GaivoronkiyMRunFuncTestsProcesses, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
@@ -77,9 +77,9 @@ const auto kTestTasksList =
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kPerfTestName = NesterovARunFuncTestsProcesses::PrintFuncTestName<NesterovARunFuncTestsProcesses>;
+const auto kPerfTestName = GaivoronkiyMRunFuncTestsProcesses::PrintFuncTestName<GaivoronkiyMRunFuncTestsProcesses>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, NesterovARunFuncTestsProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(PicMatrixTests, GaivoronkiyMRunFuncTestsProcesses, kGtestValues, kPerfTestName);
 
 }  // namespace
 
--- tasks/gaivoronskiy_m_average_vector_sum/tests/performance/main.cpp
+++ tasks/gaivoronskiy_m_average_vector_sum/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace gaivoronskiy_m_average_vector_sum {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class GaivoronskiyRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(GaivoronskiyRunPerfTestProcesses, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = GaivoronskiyRunPerfTestProcesses::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, GaivoronskiyRunPerfTestProcesses, kGtestValues, kPerfTestName);
 
 }  // namespace gaivoronskiy_m_average_vector_sum
