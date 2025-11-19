fb1f8cb59aa5407e0b009ebd915d6a7f7ec5a65a
hakolr <m.gayvoronskiy@flang.io>
2025-11-19 04:30:37 +0300

    fix: removed getenv

--- tasks/gaivoronskiy_m_average_vector_sum/tests/performance/main.cpp
+++ tasks/gaivoronskiy_m_average_vector_sum/tests/performance/main.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <array>
+#include <charconv>
 #include <cmath>
-#include <cstdlib>
 #include <numeric>
+#include <string>
+#include <string_view>
 #include <vector>
 
 #include "gaivoronskiy_m_average_vector_sum/common/include/common.hpp"
@@ -12,10 +15,16 @@
 
 namespace gaivoronskiy_m_average_vector_sum {
 
+namespace {
+
+constexpr std::array<std::size_t, 4> kPerfSizes = {1'000'000, 5'000'000, 20'000'000, 100'000'000};
+
+}
+
 class GaivoronskiyRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
   void SetUp() override {
-    const std::size_t data_size = ResolveInputSize();
+    const std::size_t data_size = ResolveInputSizeFromTestParam();
     input_data_.resize(data_size);
     for (std::size_t i = 0; i < data_size; ++i) {
       input_data_[i] = static_cast<double>((i % 101) - 50);
@@ -33,19 +42,20 @@ class GaivoronskiyRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InTy
   }
 
  private:
-  static std::size_t ResolveInputSize() {
-    constexpr std::size_t kDefaultSize = 1'000'000;
-    const char *env_value = std::getenv("PPC_AVG_VEC_SIZE");
-    if (env_value == nullptr) {
-      return kDefaultSize;
+  std::size_t ResolveInputSizeFromTestParam() {
+    const auto &test_name = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kNameTest)>(GetParam());
+    constexpr std::string_view kSizeTag = "_size";
+    const auto pos = test_name.rfind(kSizeTag);
+    if (pos == std::string::npos) {
+      return kPerfSizes.front();
     }
-    char *end_ptr = nullptr;
-    const unsigned long long parsed = std::strtoull(env_value, &end_ptr, 10);
-    if (end_ptr == env_value || parsed == 0) {
-      return kDefaultSize;
+    const auto suffix = std::string_view(test_name).substr(pos + kSizeTag.size());
+    std::size_t parsed_value = 0;
+    const auto result = std::from_chars(suffix.data(), suffix.data() + suffix.size(), parsed_value);
+    if (result.ec != std::errc() || parsed_value == 0) {
+      return kPerfSizes.front();
     }
-    const auto size = static_cast<std::size_t>(parsed);
-    return size == 0 ? kDefaultSize : size;
+    return parsed_value;
   }
 
   static double CalculateAverage(const InType &values) {
@@ -61,9 +71,37 @@ TEST_P(GaivoronskiyRunPerfTestProcesses, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, GaivoronskiyMAverageVecSumMPI, GaivoronskiyMAverageVecSumSEQ>(
-        PPC_SETTINGS_gaivoronskiy_m_average_vector_sum);
+namespace {
+
+template <typename TaskType>
+auto MakePerfTaskTuplesForSize(std::size_t size, const std::string &settings_path) {
+  const auto base_name = std::string(ppc::util::GetNamespace<TaskType>()) + "_" +
+                         ppc::task::GetStringTaskType(TaskType::GetStaticTypeOfTask(), settings_path);
+  const auto size_suffix = "_size" + std::to_string(size);
+  const auto task_name = base_name + size_suffix;
+  return std::make_tuple(std::make_tuple(ppc::task::TaskGetter<TaskType, InType>, task_name,
+                                         ppc::performance::PerfResults::TypeOfRunning::kPipeline),
+                         std::make_tuple(ppc::task::TaskGetter<TaskType, InType>, task_name,
+                                         ppc::performance::PerfResults::TypeOfRunning::kTaskRun));
+}
+
+template <typename TaskType, std::size_t N, std::size_t... I>
+auto MakePerfTasksForAllSizesImpl(const std::array<std::size_t, N> &sizes, const std::string &settings_path,
+                                  std::index_sequence<I...>) {
+  return std::tuple_cat(MakePerfTaskTuplesForSize<TaskType>(sizes[I], settings_path)...);
+}
+
+template <typename TaskType, std::size_t N>
+auto MakePerfTasksForAllSizes(const std::array<std::size_t, N> &sizes, const std::string &settings_path) {
+  return MakePerfTasksForAllSizesImpl<TaskType>(sizes, settings_path, std::make_index_sequence<N>{});
+}
+
+}  // namespace
+
+const auto kAllPerfTasks = std::tuple_cat(
+    MakePerfTasksForAllSizes<GaivoronskiyMAverageVecSumMPI>(kPerfSizes, PPC_SETTINGS_gaivoronskiy_m_average_vector_sum),
+    MakePerfTasksForAllSizes<GaivoronskiyMAverageVecSumSEQ>(kPerfSizes,
+                                                            PPC_SETTINGS_gaivoronskiy_m_average_vector_sum));
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
